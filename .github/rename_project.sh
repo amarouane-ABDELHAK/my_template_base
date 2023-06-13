51c1c2329f0e9610300d1b51844d52e2c4802477
amarouane-ABDELHAK <amarouane-ABDELHAK@users.noreply.github.com>
2023-06-13 14:21:35 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="amarouane-ABDELHAK"
+original_name="my_template_base"
+original_urlname="my_template_base"
+original_description="Awesome my_template_base created by amarouane-ABDELHAK"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv my_template_base $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from my_template_base.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "my_template_base"
